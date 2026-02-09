cd8a68cf5f21d5a79e220f2ea75877e8529941a6
bryan-senai09 <bryan.o.teixeira@aluno.senai.br>
2026-02-09 11:36:28 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿// Console.WriteLine("coloque o nome ");
+// string nome = Console.ReadLine();
+
+
+// Console.WriteLine("agora adicione outra idade");
+// int idade2 = int.Parse(Console.ReadLine());
+
+
+// Console.WriteLine("coloque outro nome");
+// string nome2 = Console.ReadLine();
+
+// Console.WriteLine("agora adicione outra idade");
+// int idade2 = int.Parse(Console.ReadLine());
+
+// if(idade1 > idade2)
+// {
+    
+// Console.WriteLine( nome1 + "  A pessoa e o mais velha");
+
+
+
+// }
+// {
+
+// else if (idade1 > idade2)
+
+// }
+
+//     Console.WriteLine("os dois tem a mesma idade");
+
+
+// else
+// {
+//     Console.WriteLine( nome2 + "  A pessoa e mais velha");
+// }
+
+
+// questao 2-
+
+
+// Console.WriteLine("coloque o primeiro numero ");
+// int n1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("coloque o segundo numero ");
+// int n2 = int.Parse(Console.ReadLine());
+
+
+// double soma = n1 + n2;
+// Console.WriteLine("Os numeros somados sao " + soma);
+
+// double subtraçao = n1 - n2;
+// Console.WriteLine("Os numeros subtraidos sao " + subtraçao);
+
+// double multiplicaçao = n1 * n2;
+// Console.WriteLine("Os numeros multiplicados sao " + multiplicaçao);
+
+// double divisao = n1 / n2;
+// Console.WriteLine("Os numeros divididos sao " + divisao);
+
+
+
+// Console.WriteLine("coloque um numero");
+// int numero = int.Parse(Console.ReadLine());
+
+// int dobro = numero * 2;
+// Console.WriteLine("o dobro do numero e " + dobro);
+
+// int triplo = numero * 3;
+// Console.WriteLine("o triplo do numero e " + triplo);
+
+
+
+// Console.WriteLine("coloque seu nome");
+// string nome = Console.ReadLine();
+
+// Console.WriteLine("bem vindo"+ nome);
+
+// Console.WriteLine("coloque sua idade");
+// int idade = int.Parse(Console.ReadLine());
+
+// int idadefutura = idade + 10;
+
+// Console.WriteLine(" sua idade daqui 10 anos vai ser " + idadefutura + " anos");
+
+
+// Ativ-2
+
+
+
+// Console.WriteLine(" escolha um numero");
+// int numero = int.Parse(Console.ReadLine());
+
+    
+// if (numero % 2==0)
+// {
+    
+//     Console.WriteLine(" o numero e par");
+// }
+// else
+// {
+//     Console.WriteLine("o numero e impar");
+// }
+
+
+// Console.WriteLine("");
+// int numero1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("");
+// int numero2 = int.Parse(Console.ReadLine());
+
+// if (numero1 > numero2)
+// {
+//     Console.WriteLine("qual e numero maior");
+// }
+
+
+Console.WriteLine("coloque  sua nota");
+double nota = double.Parse(Console.ReadLine());
+
+if (nota > 6)
+{
+    Console.WriteLine("Muito bem vc foi aprovado");
+}
+else if (nota > 5)
+{
+    
+Console.WriteLine("ai vc esta de recuperaçao");
+
+}
+else
+{
+    Console.WriteLine("ai melhore da poxima vez");
+}
